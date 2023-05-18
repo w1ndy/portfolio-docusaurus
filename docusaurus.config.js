51ad2bf9866d2165cd6ca5f454233f5fa3fb825d
Di Weng <diweng@microsoft.com>
2023-05-18 19:54:42 +0800

    add google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-32NCZNJ8GJ",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
